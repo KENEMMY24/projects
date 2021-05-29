bb76e28304a63ba31078c56d2368e1c94797d242
KENEMMY24 <84340793+KENEMMY24@users.noreply.github.com>
2021-05-29 13:04:39 +0300

    area of a circle
    a program to find out the area of a circle given the radius

---
+++ area of circle.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	const float PIE=3.14;
+	float radius;
+
+	
+	printf("Enter the radius of a circle to find its area: ");
+	scanf("%f", &radius);
+	
+	printf("The area of the circle is: %f", PIE*radius*radius);
+	
+	
+	
+	
+}
