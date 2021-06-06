da106fcda1cff561bfdee7d085f6d46771295dc3
KENEMMY24 <84340793+KENEMMY24@users.noreply.github.com>
2021-06-06 16:54:21 +0300

    printing n

---
+++ value of n.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int m;
+	
+	
+	printf("Enter an integer:\n");
+	scanf("%d", &m);
+	
+	if(m<0){
+	   printf("The value of n= -1.");
+	   return 0;23
+	} 
+	if(m>0){
+		printf("The value of n= 1.");
+	}
+	else{
+		printf("The value of n= 0.");
+	}
+}
